2a2e2dd82d0c153d65991deb544506e77a98fcf5
markeisenberg <markeisenberg43@gmail.com>
2016-04-17 15:13:10 +0200

    Added WebView, changed visibility of ScrollView to 'gone' in XML, added Toast function to handle the display of toasts, added comments and changed variable/class names

--- app/src/main/java/com/example/desperados/ex1_http/MainActivity.java
+++ app/src/main/java/com/example/desperados/ex1_http/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.desperados.ex1_http;
 
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.webkit.WebView;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +16,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,13 +26,17 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+// This code based on the code found here:
+// http://developer.android.com/training/basics/network-ops/connecting.html
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String DEBUG_TAG = "HttpExample";
+    private static String toastText = "";
     private static EditText et_inputHTTP;
     private static TextView tv_outputContent;
     private static Button  btn_getHTTP;
+    private static WebView webDisplay;
 
 
 
@@ -39,7 +47,9 @@ public class MainActivity extends AppCompatActivity {
         et_inputHTTP = (EditText)findViewById(R.id.et_inputHTTP);
         tv_outputContent = (TextView) findViewById(R.id.tv_outputContent);
         btn_getHTTP = (Button) findViewById(R.id.btn_getHTTP);
-        
+        webDisplay = (WebView) findViewById(R.id.wv_WebContent);
+
+        // Prevents the keyboard from opening automatically on app start
         btn_getHTTP.setFocusable(true);
         btn_getHTTP.setFocusableInTouchMode(true);
         btn_getHTTP.requestFocus();
@@ -54,31 +64,50 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //Handles toasts for error messages. Based on code from: http://stackoverflow.com/a/23936088
+    public void showToast(){
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            public void run() {
+                Toast myToast = Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_LONG);
+                myToast.show();
+            }
+        });
+    }
+
     public void buttonClicked(View view) {
         String url = et_inputHTTP.getText().toString();
+
+        // Uses context to make use of system services available and establishes a connection
         ConnectivityManager connMan = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
 
+        //Checks for a network connection
         NetworkInfo netInfo = connMan.getActiveNetworkInfo();
         if (netInfo != null && netInfo.isConnected()) {
-            new DownloadMaster().execute(url);
+            new ContentRetrieval().execute(url);
             //tv_outputContent.setText("Connected to the network");
+            webDisplay.loadUrl(url);
         } else {
-            tv_outputContent.setText("No network connection available.");
+            //tv_outputContent.setText("No network connection available.");
+
+            toastText = "No network connection available";
+            showToast();
         }
     }
 
-    private class DownloadMaster extends AsyncTask<String, Void, String> {
+    private class ContentRetrieval extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
 
+            // app checks if it can find the web address
             try {
                 return AccessUrl(urls[0]);
             } catch (IOException e) {
                 return "Unable to reach the page, please check the URL";
             }
         }
-        // onPostExecute displays the results of the AsyncTask.
+        // onPostExecute displays the results of the AsyncTask i.e. our HTML content.
         @Override
         protected void onPostExecute(String outputAsync) {
             tv_outputContent.setText(outputAsync);
@@ -87,58 +116,76 @@ public class MainActivity extends AppCompatActivity {
 
 
     private String AccessUrl(String address) throws IOException {
-        InputStream is = null;
+        InputStream webContent = null;
+
         // Only display the first 500 characters of the retrieved
         // web page content.
-        int length = 500;
+        int charsToDisplay = 500;
 
         try {
             URL userUrl = new URL(address);
-            HttpURLConnection connect = (HttpURLConnection) userUrl.openConnection();
-            connect.setReadTimeout(10000 /* milliseconds */);
-            connect.setConnectTimeout(15000 /* milliseconds */);
-            connect.setRequestMethod("GET");
-            connect.setDoInput(true);
+            HttpURLConnection connectMe = (HttpURLConnection) userUrl.openConnection();
+            connectMe.setReadTimeout(10000 /* milliseconds */);
+            connectMe.setConnectTimeout(15000 /* milliseconds */);
+            connectMe.setRequestMethod("GET");
+            connectMe.setDoInput(true);
             // Starts the query
-            connect.connect();
-            int response = connect.getResponseCode();
+            connectMe.connect();
+            int response = connectMe.getResponseCode();
 
-            switch (connect.getResponseCode()) {
+            //different things happen depending on the response code received
+            switch (connectMe.getResponseCode()) {
                 case HttpURLConnection.HTTP_OK:
                     Log.d(DEBUG_TAG, " **OK**");
-                    break; // fine, go on
+
+                    toastText = "Connection successful";
+                    showToast();
+
+                    break; // continue
                 case HttpURLConnection.HTTP_GATEWAY_TIMEOUT:
                     Log.d(DEBUG_TAG, " **gateway timeout**");
+
+                    toastText = "Timeout";
+                    showToast();
                     break;// retry
                 case HttpURLConnection.HTTP_UNAVAILABLE:
-                    System.out.println(DEBUG_TAG + "**unavailable**");
-                    break;// retry, server is unstable
+                    Log.d(DEBUG_TAG, "**unavailable**");
+
+                    toastText = "Server unavailable";
+                    showToast();
+                    break;// problem server side, retry
                 default:
                     Log.d(DEBUG_TAG, " **unknown response code**.");
+
+                    toastText = "Unknown response code received";
+                    showToast();
                     break; // abort
             }
 
 
             //Log.d(DEBUG_TAG, "The response is: " + response);
-            is = connect.getInputStream();
+            webContent = connectMe.getInputStream();
 
-            // Convert the InputStream into a string
-            String contentAsString = readIt(is, length);
-            return contentAsString;
+            // Convert the InputStream into a string and display the max chars as set before
+            String contentToString = useAppReader(webContent, charsToDisplay);
+            return contentToString;
 
-            // Makes sure that the InputStream is closed after the app is
-            // finished using it.
+            // Once information is retrieved, close InputStream
         } finally {
-            if (is != null) {
-                is.close();
+            if (webContent != null) {
+                webContent.close();
             }
         }
     }
-    public String readIt(InputStream stream, int length) throws IOException, UnsupportedEncodingException {
-        Reader reader = null;
-        reader = new InputStreamReader(stream, "UTF-8");
-        char[] buffer = new char[length];
-        reader.read(buffer);
-        return new String(buffer);
+    /*
+    use the java Reader to convert the input data to characters and return string
+    and allocate space (buffer) in memory to the stream
+    */
+    public String useAppReader(InputStream receivedData, int length) throws IOException, UnsupportedEncodingException {
+        Reader showContent = null;
+        showContent = new InputStreamReader(receivedData, "UTF-8");
+        char[] allocateBuffer = new char[length];
+        showContent.read(allocateBuffer);
+        return new String(allocateBuffer);
     }
 }
